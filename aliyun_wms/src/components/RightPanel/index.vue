2020ed13091cdb8b74f653ee5871c7f536127c42
李岩 <541267924@qq.com>
2019-08-15 21:42:17 +0800

    添加侧边栏

--- aliyun_wms/src/components/RightPanel/index.vue
+++ aliyun_wms/src/components/RightPanel/index.vue
@@ -1,23 +1,26 @@
 <template>
   <div ref="rightPanel" :class="{show:show}" class="rightPanel-container">
-    1111
-    <!-- <div class="rightPanel-background" />
+    <div class="rightPanel-background" />
     <div class="rightPanel">
-      <div class="handle-button" :style="{'top':buttonTop+'px','background-color':theme}" @click="show=!show">
+      <div
+        class="handle-button"
+        :style="{'top':buttonTop+'px','background-color':theme}"
+        @click="show=!show"
+      >
         <i :class="show?'el-icon-close':'el-icon-setting'" />
       </div>
       <div class="rightPanel-items">
         <slot />
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script>
-import { addClass, removeClass } from '@/utils'
+import { addClass, removeClass } from "@/utils";
 
 export default {
-  name: 'RightPanel',
+  name: "RightPanel",
   props: {
     clickNotClose: {
       default: false,
@@ -31,50 +34,50 @@ export default {
   data() {
     return {
       show: false
-    }
+    };
   },
   computed: {
     theme() {
-      return this.$store.state.settings.theme
+      return "red";
     }
   },
   watch: {
     show(value) {
       if (value && !this.clickNotClose) {
-        this.addEventClick()
+        this.addEventClick();
       }
       if (value) {
-        addClass(document.body, 'showRightPanel')
+        addClass(document.body, "showRightPanel");
       } else {
-        removeClass(document.body, 'showRightPanel')
+        removeClass(document.body, "showRightPanel");
       }
     }
   },
   mounted() {
-    this.insertToBody()
+    this.insertToBody();
   },
   beforeDestroy() {
-    const elx = this.$refs.rightPanel
-    elx.remove()
+    const elx = this.$refs.rightPanel;
+    elx.remove();
   },
   methods: {
     addEventClick() {
-      window.addEventListener('click', this.closeSidebar)
+      window.addEventListener("click", this.closeSidebar);
     },
     closeSidebar(evt) {
-      const parent = evt.target.closest('.rightPanel')
+      const parent = evt.target.closest(".rightPanel");
       if (!parent) {
-        this.show = false
-        window.removeEventListener('click', this.closeSidebar)
+        this.show = false;
+        window.removeEventListener("click", this.closeSidebar);
       }
     },
     insertToBody() {
-      const elx = this.$refs.rightPanel
-      const body = document.querySelector('body')
-      body.insertBefore(elx, body.firstChild)
+      const elx = this.$refs.rightPanel;
+      const body = document.querySelector("body");
+      body.insertBefore(elx, body.firstChild);
     }
   }
-}
+};
 </script>
 
 <style>
@@ -91,8 +94,8 @@ export default {
   top: 0;
   left: 0;
   opacity: 0;
-  transition: opacity .3s cubic-bezier(.7, .3, .1, 1);
-  background: rgba(0, 0, 0, .2);
+  transition: opacity 0.3s cubic-bezier(0.7, 0.3, 0.1, 1);
+  background: rgba(0, 0, 0, 0.2);
   z-index: -1;
 }
 
@@ -102,46 +105,43 @@ export default {
   height: 100vh;
   position: fixed;
   top: 0;
-  right: 0;
-  box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, .05);
-  transition: all .25s cubic-bezier(.7, .3, .1, 1);
-  transform: translate(100%);
+  left: 0;
+  box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.05);
+  transition: all 0.25s cubic-bezier(0.7, 0.3, 0.1, 1);
+  transform: translate(-100%);
   background: #fff;
   z-index: 40000;
 }
 
 .show {
-  transition: all .3s cubic-bezier(.7, .3, .1, 1);
-
-
+  transition: all 0.3s cubic-bezier(0.7, 0.3, 0.1, 1);
 }
 .show .rightPanel-background {
-    z-index: 20000;
-    opacity: 1;
-    width: 100%;
-    height: 100%;
-  }
+  z-index: 20000;
+  opacity: 1;
+  width: 100%;
+  height: 100%;
+}
 
-  .show .rightPanel {
-    transform: translate(0);
-  }
+.show .rightPanel {
+  transform: translate(0);
+}
 .handle-button {
   width: 48px;
   height: 48px;
   position: absolute;
-  left: -48px;
+  right: -48px;
   text-align: center;
   font-size: 24px;
-  border-radius: 6px 0 0 6px !important;
+  border-radius:0 6px 6px 0!important;
   z-index: 0;
   pointer-events: auto;
   cursor: pointer;
   color: #fff;
   line-height: 48px;
-
 }
 .handle-button i {
-    font-size: 24px;
-    line-height: 48px;
-  }
+  font-size: 24px;
+  line-height: 48px;
+}
 </style>
