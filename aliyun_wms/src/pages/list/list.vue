1139a2b1493018b0a8a6dccd37db19a42c7538da
liyan <541267924@qq.com>
2019-10-09 10:46:54 +0800

    提交最新

--- aliyun_wms/src/pages/list/list.vue
+++ aliyun_wms/src/pages/list/list.vue
@@ -1,21 +1,51 @@
 <template>
-  <div class="container">
-    我是list
-  </div>
+	<div class="container">
+		我是list
+		<div class="div-search"></div>
+		<div class="div-table">
+			<el-table
+				:data="tableData"
+				style="width: 100%"
+			>
+				<el-table-column prop="date" label="日期" width="180">
+				</el-table-column>
+				<el-table-column prop="name" label="姓名" width="180">
+				</el-table-column>
+				<el-table-column prop="address" label="地址"> </el-table-column>
+			</el-table>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'List',
-  data () {
-    return {
-
-    }
-  }
+	name: 'List',
+	data () {
+		return {
+			tableData: [{
+				date: '2016-05-02',
+				name: '王小虎',
+				address: '上海市普陀区金沙江路 1518 弄',
+			}, {
+				date: '2016-05-04',
+				name: '王小虎',
+				address: '上海市普陀区金沙江路 1518 弄'
+			}, {
+				date: '2016-05-01',
+				name: '王小虎',
+				address: '上海市普陀区金沙江路 1518 弄',
+			}, {
+				date: '2016-05-03',
+				name: '王小虎',
+				address: '上海市普陀区金沙江路 1518 弄'
+			}]
+		}
+	},
+	methods: {
+	},
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped>
-
 </style>
