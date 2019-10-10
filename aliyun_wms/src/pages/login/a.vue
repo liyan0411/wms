cf7cd08fbd4f56203069647ed15417c47b03f346
liyan <541267924@qq.com>
2019-10-10 16:31:07 +0800

    登录页

---
+++ aliyun_wms/src/pages/login/a.vue
@@ -0,0 +1,335 @@
+<template>
+	<div class="login-bg">
+		<div class="login-left-bg" :style="fullWidth">
+			<div class="icon-logo">
+				<!-- <img src="../../common/img/logo_home.png" alt=""> -->
+			</div>
+			<div class="logo-text">
+				<p>MANAGEMENT BACKGROUND</p>
+				<P>昕斗云</P>
+			</div>
+		</div>
+		<div class="login-right">
+			<div class="login-box">
+				<div class="login-logo">
+					<span>欢迎登录昕斗云</span>
+				</div>
+				<el-form
+					:model="login"
+					:rules="loginRules"
+					ref="ruleForm"
+					class="demo-ruleForm"
+				>
+					<el-form-item prop="name">
+						<el-input v-model="login.name" placeholder="用户名">
+							<p slot="prepend">
+								<img src="../../assets/imgs/icon_head.png" alt="" />
+							</p>
+						</el-input>
+					</el-form-item>
+					<el-form-item prop="password">
+						<el-input v-model="login.password" placeholder="密码">
+							<p slot="prepend">
+								<img src="../../assets/imgs/icon_password.png" alt="" />
+							</p>
+						</el-input>
+					</el-form-item>
+					<el-form-item>
+						<span class="forget-pw pointer" @click="forget">忘记密码？</span>
+					</el-form-item>
+				</el-form>
+
+				<div class="loginbutonDiv">
+					<el-button
+						class="buttonSize button-solid"
+						type="primary"
+						@click="loginFun('ruleForm')"
+						>登录</el-button
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import md5 from 'js-md5';
+import {
+	Message
+} from 'element-ui'
+
+export default {
+	data () {
+		return {
+			login: {
+				name: "",
+				password: ""
+			},
+			loginRules: {
+				name: [
+					{ required: true, message: '请输入用户名', trigger: 'blur' }
+				],
+				password: [
+					{ required: true, message: '请输入密码', trigger: 'blur' }
+				]
+			},
+			fullWidth: { // 动态获取屏幕高度
+				width: (document.documentElement.clientWidth - 545) + "px"
+			},
+			borderType: ""
+		}
+	},
+	methods: {
+		iconClick (item) {
+			if (item == 'username') {
+				document.getElementById('usernameIput').focus();
+			} else {
+				document.getElementById('passwordIput').focus();
+			}
+		},
+		focusFun (val) {
+			let self = this;
+			self.borderType = val;
+		},
+		blurFun () {
+			let self = this;
+			self.borderType = "";
+		},
+		loginFun (formName) {
+			let self = this;
+			console.log(self.login)
+			this.$refs[formName].validate((valid) => {
+				if (valid) {
+
+					self.$router.push("/home");
+				} else {
+					console.log('error submit!!');
+					return false;
+				}
+			});
+		},
+		forget () {
+			console.log("忘记密码")
+		}
+	},
+	mounted () {
+		var self = this;
+		document.onkeydown = function (e) {
+			var ev = document.all ? window.event : e;
+			if (ev.keyCode == 13) {
+				if (self.$route.path == "/login" || self.$route.path == "/") {
+					self.loginFun();
+					return false;
+				}
+
+			}
+		};
+		window.onresize = () => {
+			return (() => {
+				self.fullWidth.width = (document.documentElement.clientWidth - 516) + "px"
+			})()
+		}
+	}
+}
+
+</script>
+
+<style scoped>
+.login-bg {
+	width: 100%;
+	height: 100%;
+}
+
+.login-left-bg {
+	height: 100%;
+	background: url(../../assets/imgs/icon_loginbg.jpg) 80% 100%;
+	background-repeat: no-repeat;
+	/*占满屏幕,auto是随尺寸*/
+	background-size: cover;
+}
+
+.icon-logo {
+	position: absolute;
+	top: 50px;
+	left: 50px;
+}
+
+.logo-text {
+	font-family: PingFangSC-Semibold;
+	color: #ffffff;
+	text-align: right;
+	position: absolute;
+
+	bottom: 100px;
+	right: 750px;
+}
+
+.logo-text > p:first-child {
+	line-height: 67px;
+	font-size: 48px;
+}
+
+.logo-text > p:last-child {
+	padding-right: 30px;
+	line-height: 40px;
+	font-size: 28px;
+}
+
+/*400*300*/
+
+.login-right {
+	width: 695px;
+	height: 100%;
+	/* background-color: red; */
+	background: url(../../assets/imgs/loginbg_white.png) 100%;
+	background-attachment: fixed;
+	background-repeat: no-repeat;
+	/*占满屏幕,auto是随尺寸*/
+	/*background-size: cover;*/
+	position: fixed;
+	right: 0;
+	top: 0;
+}
+
+.login-box {
+	width: 330px;
+	height: 300px;
+	padding: 25px;
+	position: absolute;
+	top: 50%;
+	right: 50%;
+	margin: -210px -240px 0 0;
+	border-radius: 5px;
+	background-color: rgba(255, 255, 255, 0.3);
+
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.15);
+}
+
+.login-logo {
+	width: 220px;
+	/*height: 120px;*/
+	margin: 0 auto 80px;
+	text-align: center;
+	font-family: PingFangSC-Medium;
+	font-size: 28px;
+	color: #333333;
+	letter-spacing: 2.33px;
+}
+
+.login-logo span {
+	font-family: PingFangSC-Medium;
+	font-size: 28px;
+	color: #333333;
+	letter-spacing: 2.33px;
+	padding-bottom: 3px;
+	border-bottom: 4px solid #7d82c4;
+}
+
+/*输入框*/
+
+.input-box {
+	width: 320px;
+	height: 40px;
+	line-height: 40px;
+	margin-bottom: 20px;
+	background: white;
+	margin: 0 auto 20px;
+	/*		border-radius: 5px;*/
+	overflow: hidden;
+	position: relative;
+	border-bottom: 2px solid #e6e6e6;
+}
+
+.input-color {
+	border-color: #7986e1;
+}
+
+.input-box > input {
+	width: 85%;
+	height: 100%;
+	background: none;
+	color: #666;
+	font-size: 14px;
+	line-height: inherit;
+	padding-left: 15%;
+	display: inline-block;
+	/*去除谷歌输入框的黄色背景*/
+	-webkit-box-shadow: 0 0 0px 1000px white inset;
+	overflow: hidden;
+}
+
+.icon-username {
+	position: absolute;
+	width: 15%;
+	height: 40px;
+	background: url(../../assets/imgs/icon_head.png) no-repeat center;
+	display: inline-block;
+}
+
+.icon-password {
+	position: absolute;
+	width: 15%;
+	height: 40px;
+	background: url(../../assets/imgs/icon_password.png) no-repeat center;
+	display: inline-block;
+}
+
+.loginbutonDiv {
+	position: absolute;
+	bottom: -23px;
+	right: 50%;
+	text-align: center;
+	margin-right: -160px;
+}
+
+.buttonSize {
+	width: 320px;
+	height: 46px;
+  border-radius: 6px;
+  font-size: 16px;
+  letter-spacing: 20px;
+}
+
+/*实心*/
+
+/* .button-solid {
+	background: #7d82c4;
+	color: white;
+	border-color: #7d82c4;
+}
+
+.button-solid.is-active,
+.button-solid:active {
+	background: #7d82c4;
+	border-color: #7d82c4;
+	color: white;
+}
+
+.button-solid:focus,
+.button-solid:hover {
+	background: #7d82c4;
+	border-color: #7d82c4;
+	color: white;
+} */
+.forget-pw {
+	color: #7d82c4;
+}
+</style>
+
+<style lang="scss">
+.login-box .el-form-item--small.el-form-item {
+	margin-bottom: 25px !important;
+}
+.login-box .el-input--small .el-input__inner {
+	height: 40px !important;
+	// border:none;
+}
+.login-box .el-input-group__append,
+.login-box .el-input-group__prepend {
+	background: transparent;
+	// border:none;
+}
+.login-box .el-form-item--small.el-form-item:last-child {
+	margin-bottom: 0 !important;
+}
+</style>
--- aliyun_wms/src/pages/login/index.vue
+++ aliyun_wms/src/pages/login/index.vue
@@ -1,51 +1,56 @@
 <template>
 	<div class="login-bg">
-		<div class="login-left-bg" :style="fullWidth">
-			<div class="icon-logo">
-				<!-- <img src="../../common/img/logo_home.png" alt=""> -->
-			</div>
-			<div class="logo-text">
-				<p>MANAGEMENT BACKGROUND</p>
-				<P>昕斗云</P>
-			</div>
+		<div class="zm">
+			<img src="../../assets/imgs/login-zimu-w.png" alt="" />
 		</div>
-		<div class="login-right">
-			<div class="login-box">
-				<div class="login-logo">
-					<span>欢迎登录昕斗云</span>
-				</div>
-				<el-form
-					:model="login"
-					:rules="loginRules"
-					ref="ruleForm"
-					class="demo-ruleForm"
-				>
-					<el-form-item prop="name">
-						<el-input v-model="login.name" placeholder="用户名">
-							<p slot="prepend">
-								<img src="../../assets/imgs/icon_head.png" alt="" />
-							</p>
-						</el-input>
-					</el-form-item>
-					<el-form-item prop="password">
-						<el-input v-model="login.password" placeholder="密码">
-							<p slot="prepend">
-								<img src="../../assets/imgs/icon_password.png" alt="" />
-							</p>
-						</el-input>
-					</el-form-item>
-					<el-form-item>
-						<span class="forget-pw pointer" @click="forget">忘记密码？</span>
-					</el-form-item>
-				</el-form>
-
-				<div class="loginbutonDiv">
-					<el-button
-						class="buttonSize button-solid"
-						type="primary"
-						@click="loginFun('ruleForm')"
-						>登录</el-button
+		<div class="login-content">
+			<div class="login-right">
+				<div class="login-box">
+					<div class="title">
+						<h2>昕斗云</h2>
+					</div>
+					<el-form
+						:model="login"
+						:rules="loginRules"
+						ref="ruleForm"
+						class="demo-ruleForm"
 					>
+						<p class="tip" :class="borderType == 'name' ? 'tip-active' : ''">
+							用户名
+						</p>
+						<el-form-item prop="name">
+							<el-input
+								v-model="login.name"
+								placeholder="请输入"
+								@focus="focusFun('name')"
+							>
+							</el-input>
+						</el-form-item>
+						<p
+							class="tip"
+							:class="borderType == 'password' ? 'tip-active' : ''"
+						>
+							密码
+						</p>
+						<el-form-item prop="password">
+							<el-input
+								v-model="login.password"
+								placeholder="请输入"
+								@focus="focusFun('password')"
+							>
+							</el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-checkbox v-model="checked">记住密码</el-checkbox>
+							<span class="forget-pw pointer" @click="forget">忘记密码？</span>
+						</el-form-item>
+					</el-form>
+
+					<div class="loginbutonDiv">
+						<el-button class="btn" type="primary" @click="loginFun('ruleForm')"
+							>登录</el-button
+						>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -65,6 +70,7 @@ export default {
 				name: "",
 				password: ""
 			},
+			checked: false,
 			loginRules: {
 				name: [
 					{ required: true, message: '请输入用户名', trigger: 'blur' }
@@ -134,200 +140,117 @@ export default {
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .login-bg {
 	width: 100%;
 	height: 100%;
-}
-
-.login-left-bg {
-	height: 100%;
-	background: url(../../assets/imgs/icon_loginbg.jpg) 80% 100%;
-	background-repeat: no-repeat;
-	/*占满屏幕,auto是随尺寸*/
-	background-size: cover;
-}
-
-.icon-logo {
-	position: absolute;
-	top: 50px;
-	left: 50px;
-}
-
-.logo-text {
-	font-family: PingFangSC-Semibold;
-	color: #ffffff;
-	text-align: right;
-	position: absolute;
-
-	bottom: 100px;
-	right: 750px;
-}
-
-.logo-text > p:first-child {
-	line-height: 67px;
-	font-size: 48px;
-}
-
-.logo-text > p:last-child {
-	padding-right: 30px;
-	line-height: 40px;
-	font-size: 28px;
-}
-
-/*400*300*/
-
-.login-right {
-	width: 695px;
-	height: 100%;
-	/* background-color: red; */
-	background: url(../../assets/imgs/loginbg_white.png) 100%;
-	background-attachment: fixed;
-	background-repeat: no-repeat;
-	/*占满屏幕,auto是随尺寸*/
-	/*background-size: cover;*/
-	position: fixed;
-	right: 0;
-	top: 0;
-}
-
-.login-box {
-	width: 330px;
-	height: 300px;
-	padding: 25px;
-	position: absolute;
-	top: 50%;
-	right: 50%;
-	margin: -210px -240px 0 0;
-	border-radius: 5px;
-	background-color: rgba(255, 255, 255, 0.3);
-
-	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.15);
-}
-
-.login-logo {
-	width: 220px;
-	/*height: 120px;*/
-	margin: 0 auto 80px;
-	text-align: center;
-	font-family: PingFangSC-Medium;
-	font-size: 28px;
-	color: #333333;
-	letter-spacing: 2.33px;
-}
-
-.login-logo span {
-	font-family: PingFangSC-Medium;
-	font-size: 28px;
-	color: #333333;
-	letter-spacing: 2.33px;
-	padding-bottom: 3px;
-	border-bottom: 4px solid #7d82c4;
-}
-
-/*输入框*/
-
-.input-box {
-	width: 320px;
-	height: 40px;
-	line-height: 40px;
-	margin-bottom: 20px;
-	background: white;
-	margin: 0 auto 20px;
-	/*		border-radius: 5px;*/
-	overflow: hidden;
+	background-image: linear-gradient(
+		-43deg,
+		#5222f4 0%,
+		#646cf2 73%,
+		#74b2ef 100%
+	);
+	background: #ffffff;
 	position: relative;
-	border-bottom: 2px solid #e6e6e6;
-}
+	.zm {
+		width: 100%;
+		text-align: center;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
 
-.input-color {
-	border-color: #7986e1;
-}
+		img {
+			width: 90%;
+		}
+	}
+	.login-content {
+		width: 1200px;
+		height: 700px;
 
-.input-box > input {
-	width: 85%;
-	height: 100%;
-	background: none;
-	color: #666;
-	font-size: 14px;
-	line-height: inherit;
-	padding-left: 15%;
-	display: inline-block;
-	/*去除谷歌输入框的黄色背景*/
-	-webkit-box-shadow: 0 0 0px 1000px white inset;
-	overflow: hidden;
-}
-
-.icon-username {
-	position: absolute;
-	width: 15%;
-	height: 40px;
-	background: url(../../assets/imgs/icon_head.png) no-repeat center;
-	display: inline-block;
-}
-
-.icon-password {
-	position: absolute;
-	width: 15%;
-	height: 40px;
-	background: url(../../assets/imgs/icon_password.png) no-repeat center;
-	display: inline-block;
-}
-
-.loginbutonDiv {
-	position: absolute;
-	bottom: -23px;
-	right: 50%;
-	text-align: center;
-	margin-right: -160px;
-}
-
-.buttonSize {
-	width: 320px;
-	height: 46px;
-	border-radius: 6px;
-}
-
-/*实心*/
-
-.button-solid {
-	background: #7d82c4;
-	color: white;
-	border-color: #7d82c4;
-}
-
-.button-solid.is-active,
-.button-solid:active {
-	background: #7d82c4;
-	border-color: #7d82c4;
-	color: white;
-}
-
-.button-solid:focus,
-.button-solid:hover {
-	background: #7d82c4;
-	border-color: #7d82c4;
-	color: white;
-}
-.forget-pw {
-	color: #7d82c4;
+		background: url(../../assets/imgs/login-1.png);
+		background-repeat: no-repeat;
+		/*占满屏幕,auto是随尺寸*/
+		background-size: contain;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		.login-right {
+			padding: 60px 120px;
+			width: 50%;
+			box-sizing: border-box;
+			height: 700px;
+			float: right;
+			.title h2 {
+				font-size: 26px;
+				color: #51539f;
+				text-align: center;
+				letter-spacing: 0.67px;
+				font-weight: 600;
+				margin: 70px 0;
+			}
+			.demo-ruleForm {
+				width: 320px;
+				margin: 0 auto;
+				.tip {
+					font-family: PingFangSC-Regular;
+					font-size: 12px;
+					color: #b7b7b7;
+					padding: 0 20px;
+					border-left: 4px solid #b7b7b7;
+					margin-top: 30px;
+				}
+				.tip-active {
+					border-color: #51539f;
+				}
+				.el-form-item--small.el-form-item:last-child {
+					margin-top: 40px;
+				}
+			}
+			.forget-pw {
+				font-family: PingFangSC-Regular;
+				font-size: 13px;
+				color: #919191;
+				float: right;
+				letter-spacing: 0;
+				text-align: right;
+			}
+		}
+	}
+	.loginbutonDiv {
+		text-align: center;
+		margin-top: 60px;
+		.btn {
+			width: 180px;
+			background: #51539f;
+			box-shadow: 0 20px 51px -11px #3376e4;
+			border-radius: 100px;
+			font-family: PingFangSC-Regular;
+			font-size: 20px;
+			color: #ffffff;
+			line-height: 28px;
+		}
+	}
 }
 </style>
-
 <style lang="scss">
-.login-box .el-form-item--small.el-form-item {
-	margin-bottom: 25px !important;
+.el-input__inner {
+	background: #ffffff;
+	border: none !important;
+	border-bottom: 1px solid #e0e0e0 !important;
+	border-radius: 0 !important;
+	padding: 0 24px !important;
+	font-size: 14px !important;
+	color: #666666 !important;
 }
-.login-box .el-input--small .el-input__inner {
-	height: 40px !important;
-	// border:none;
+.el-form-item.is-error .el-input__inner,
+.el-form-item.is-error .el-input__inner:focus,
+.el-form-item.is-error .el-textarea__inner,
+.el-form-item.is-error .el-textarea__inner:focus {
+	border-color: #f56c6c !important;
 }
-.login-box .el-input-group__append,
-.login-box .el-input-group__prepend {
-	background: transparent;
-	// border:none;
-}
-.login-box .el-form-item--small.el-form-item:last-child {
-	margin-bottom: 0 !important;
+.el-input__inner:focus {
+	border-color: #7378f0 !important;
 }
 </style>
